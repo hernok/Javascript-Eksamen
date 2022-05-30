f6f1f9d7e0f934062d11da865a64a78a35e80b13
hernok <hernok@hotmail.com>
2022-05-30 20:02:57 +0200

    a few functions

--- js/index.js
+++ js/index.js
@@ -1,40 +1,106 @@
 let usersList = document.getElementById("users-list");
-let users = [];
+let currentGender = "";
+let interested = [];
+let notInterested = [];
+let male = [];
+let female = [];
+let allUsers = [];
+
+
+
+function filterByGender(genderType){
+	currentGender = genderType
+	let filteredUsers = allUsers.results.filter(function(user){
+		return(
+			user.gender == genderType
+		);
+	});
+	displayUsers(filteredUsers);
+
+	console.log(allUsers)
+}
 
 async function loadUsers() {
 	 try {
-	const objects = await fetch ("https://randomuser.me/api/?results=50");
-	users = await objects.json();
-	
- 	console.log(users.results);
-}catch (error) {
-    console.error(error);
+		const objects = await fetch ("https://randomuser.me/api/?results=50");
+		allUsers = await objects.json();
+	}catch (error) {
+   		console.error(error);
 	}
-	displayUsers();
 };
 
-function filterByGender(genderType){
-	genderType
-}
-
-function displayUsers() {
+function displayUsers(users) {
 	usersList.innerHTML = "";
-	for (let i = 0; i < users.results.length; i++){
-		let user = users.results[i];
+		let i = 0;
+		let user = users[i];
 		let userFirstName = user.name.first;
 		let userLastName = user.name.last;
-		let userGender = user.gender;
 		let userAge = user.dob.age;
-		let userPicture = "<img class='image' src='" + user.picture.medium + "'/>";
+		let userPicture = "<img class='image' src='" + user.picture.large + "'/>";
+		userElement = document.createElement("div");
+		userElement.innerHTML =
+			`<li class="user-element">
+					<div class="user-info">
+						<h2 class="name">${userFirstName} ${userLastName}</h2>
+						<h3 class="age">Age: ${userAge}</h2>
+					</div>
+					<div class="user-picture">${userPicture}</div>
+					<div id="interested-buttons">
+						<button id="not-interested-btn" onclick="notInterestedBtnFunc()">Not interested</button> 
+						<button id="interested-btn" onclick="interestedBtnFunc()">interested</button>
+					</div>
+			</li>`;
+		usersList.appendChild(userElement);
+	
+	
+};
+/*
+let interestedBtns = document.getElementById("interested-buttons");
+let notInterestedBtn = document.getElementById("not-interested-btn");
+let interestedBtn = document.getElementById("interested-buttons");
+*/
+
+function notInterestedBtnFunc(index){
+    allUsers.results.splice(index, 1);
+	filterByGender(currentGender);
+};
+
+function interestedBtnFunc(){
+	allUsers.forEach(function(move, index){
+		allUsers.splice(index, 1);
+		interested.push(move);
+	});
+};
+
+
+
+function showLocation(){
+
+};
+
+
+
+/*-----------For Loop---------
+function displayUsers(users) {
+	usersList.innerHTML = "";
+	for (let i = 0; i < users.length; i++){
+		let user = users[i];
+		let userFirstName = user.name.first;
+		let userLastName = user.name.last;
+		let userAge = user.dob.age;
+		let userPicture = "<img class='image' src='" + user.picture.large + "'/>";
 
 		userElement = document.createElement("div");
 		userElement.innerHTML =
 			`<li class="user-element">
+				<div class="user-info">
 				<h2 class="name">${userFirstName} ${userLastName}</h2>
-				<h3 class="gender">${userGender}</h2>
 				<h3 class="age">Age: ${userAge}</h2>
-				${userPicture}
+				</div>
+				<div class="user-picture">${userPicture}</div>
 			</li>`;
 		usersList.appendChild(userElement);
 	}
+	
 };
+*/
